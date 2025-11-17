5450fcf83510964d81acca794eb3b9b3f4032eae
Overlordka <kopary787@gmail.com>
2025-11-17 13:15:48 +0100

    feat: svgs fra react-icons

--- src/components/mainMenu.jsx
+++ src/components/mainMenu.jsx
@@ -1,3 +1,12 @@
+import { FaLocationDot } from "react-icons/fa6"
+import { IoIosArrowDown } from "react-icons/io"
+import { LuBell } from "react-icons/lu"
+import { SlLocationPin } from "react-icons/sl"
+import { GoHeart } from "react-icons/go"
+import { GoHeartFill } from "react-icons/go"
+import { FiHome } from "react-icons/fi"
+import { LuMessageCircle } from "react-icons/lu"
+import { FiUser } from "react-icons/fi"
 import "./MainMenu.sass"
 
 export default function MainMenu() {
@@ -6,6 +15,15 @@ export default function MainMenu() {
         <h2>
             mainMenu
         </h2>
+        <FaLocationDot />
+        <IoIosArrowDown />
+        <LuBell />
+        <SlLocationPin />
+        <GoHeart />
+        <GoHeartFill />
+        <FiHome />
+        <LuMessageCircle />
+        <FiUser />
         </> 
     )
 }
