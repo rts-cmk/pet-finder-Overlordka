74b10a09e262ffa0e266f1ee775dbef24286e35b
Overlordka <kopary787@gmail.com>
2025-11-17 12:57:34 +0100

    feat: welcome page

--- src/components/welcomePage.jsx
+++ src/components/welcomePage.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom"
 import "./WelcomePage.sass"
 
 export default function WelcomePage() {
     return(
-        <h2>
-            welcomePage
-        </h2>
+        <>
+        <section className="welcome-sec">
+            <img src="../public/Animal.svg" alt="animal" className="welcome-sec_img" />
+            <article className="welcome-sec_text-art">
+                <h2 className="welcome-sec_h2">My Pets</h2>
+                <p className="welcome-sec_p">Taking care of a pet is my favorite, it helps me to gaimr stress and fatigue.</p>
+            </article>
+            <Link to="/home" className="welcome-sec_link">Skip</Link>
+        </section>
+        </>
     )
 }
